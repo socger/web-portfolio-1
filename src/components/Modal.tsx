0c507f7eabd7d0f7e6e482ad47b78069c3efd6d8
socger <socger@gmail.com>
2025-03-10 18:52:21 +0100

    Delete spaces after SkillComponent

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -68,7 +68,7 @@ const Modal = (
                     theme={theme}
                     key={skill.id} 
                   />
-                ))}    ))}
+                ))}
               </div>
             </div>
 
