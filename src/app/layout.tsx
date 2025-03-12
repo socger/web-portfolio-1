9c6f01cb7bb6e1a1ea9576c2f9888d363b245ea8
socger <socger@gmail.com>
2025-03-12 19:54:14 +0100

    Change the type of font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,28 @@
 import type { Metadata } from "next";
- import { Geist, Geist_Mono } from "next/font/google";
-// import {Nunito} from "next/font/google";
+//  import { Geist, Geist_Mono } from "next/font/google";
+import {Nunito} from "next/font/google";
 
 import "./globals.css";
 
 import Theme from "@/components/Theme";
 import DarkLight from "@/components/DarkLight";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-
-// const nunito = Nunito({
-//   variable: '--font-nunito', // Variable CSS para usar en Tailwind u otros estilos
-//   subsets: ['latin'], // Soporte para caracteres latinos
-//   weight: ['400', '700'], // Puedes añadir más pesos (300, 500, 900, etc.)
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
 // });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+
+const nunito = Nunito({
+  variable: '--font-nunito', // Variable CSS para usar en Tailwind u otros estilos
+  subsets: ['latin'], // Soporte para caracteres latinos
+  weight: ['400', '700'], // Puedes añadir más pesos (300, 500, 900, etc.)
+});
 
 export const metadata: Metadata = {
   title: "Jerónimo Pedro Sánchez Manzano",
@@ -37,8 +37,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        // className={`${nunito.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${nunito.variable} antialiased`}
       >
         <Theme>
           <DarkLight />
